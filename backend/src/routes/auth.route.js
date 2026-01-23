74d0038c829e3510b59811e2c309e8e048e95db3
Abhinandan1403 <crazyabhi888@gmail.com>
2026-01-23 17:14:29 +0530

    made /me route for autherization check

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -10,4 +10,9 @@ router.post("/logout",logout);
 
 router.post("/onboarding", protectRoute, onboard) // protected route just see check if the user is authenticated or not
 
+//check user is logined or not
+router.get("/me", protectRoute, (req,res)=>{
+    res.status(200).json({ success: true, user:req.user });
+});
+
 export default router
