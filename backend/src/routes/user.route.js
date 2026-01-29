83bfa413178f6e761c417b24c52bc064ae94d365
Abhinandan1403 <crazyabhi888@gmail.com>
2026-01-29 13:55:19 +0530

    create api for sending friend request

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getMyFriends, getRecommendedUsers} from "../controllers/user.controller.js"
+import {getMyFriends, getRecommendedUsers, sendFriendRequest} from "../controllers/user.controller.js"
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.use(protectRoute);//apply auth middleware to all routes
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
 
 
 export default router;
